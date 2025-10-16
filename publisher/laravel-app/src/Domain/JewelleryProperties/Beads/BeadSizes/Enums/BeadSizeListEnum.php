470da72b464d12d08202d205c9ed0f1c07845522
acwstudio <acwstudio2018@gmail.com>
2025-10-17 02:21:27 +0300

    it was fixed bead size list enum

--- publisher/laravel-app/src/Domain/JewelleryProperties/Beads/BeadSizes/Enums/BeadSizeListEnum.php
+++ publisher/laravel-app/src/Domain/JewelleryProperties/Beads/BeadSizes/Enums/BeadSizeListEnum.php
@@ -6,16 +6,22 @@ namespace Domain\JewelleryProperties\Beads\BeadSizes\Enums;
 
 enum BeadSizeListEnum: int
 {
-    case S13  = 35;
-    case S14  = 40;
-    case S15  = 45;
-    case M16  = 50;
-    case M17  = 55;
-    case M18  = 60;
-    case L19  = 65;
-    case L20  = 70;
-    case L21  = 75;
-    case XL22 = 80;
-    case XL23 = 85;
-    case XL24 = 90;
+    case SIZE_35  = 35; // collar
+    case SIZE_40  = 40; // choker
+    case SIZE_45  = 45; // princess
+    case SIZE_50  = 50; // matinee
+    case SIZE_55  = 55; // matinee
+    case SIZE_60  = 60; // matinee
+    case SIZE_65  = 65; // matinee
+    case SIZE_70  = 70; // opera
+    case SIZE_75  = 75; // opera
+    case SIZE_80 = 80; // opera
+    case SIZE_85 = 85; // opera
+    case SIZE_90 = 90; // rope
+    case SIZE_95 = 95; // rope
+    case SIZE_100 = 100; // rope
+    case SIZE_105 = 105; // rope
+    case SIZE_110 = 110; // rope
+    case SIZE_115 = 115; // rope
+    case SIZE_120 = 120; // rope
 }
