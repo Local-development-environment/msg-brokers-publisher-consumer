35da231aad5aec4fb79844b868bad4a009557342
acwstudio <acwstudio2018@gmail.com>
2025-12-25 01:44:38 +0300

    I have started media refactoring

--- publisher/laravel-app/src/Domain/JewelleryGenerator/Jewelleries/InsertItems/others.sql
+++ publisher/laravel-app/src/Domain/JewelleryGenerator/Jewelleries/InsertItems/others.sql
@@ -25,3 +25,53 @@ join
     on i.jewellery_id = weights.jewellery_id
         and i.weight = weights.weight
 ;
+-- cte video
+select
+    v.jewellery_id,
+    jsonb_agg(
+        jsonb_build_object(
+            'src', vd.src,
+            'type', vt.type,
+            'ext', vt.extension
+--             'producer', p.name
+        )
+    ) videos
+from
+    jewelleries.jewelleries as jj
+        left join jw_medias.videos v on jj.id = v.jewellery_id
+        left join jw_medias.video_details vd on v.id = vd.video_id
+        left join jw_medias.video_types vt on vd.video_type_id = vt.id
+--         left join jw_medias.producers p on v.producer_id = p.id
+group by v.jewellery_id
+;
+
+with
+    cte_videos as (
+        select
+            jsonb_agg(
+                jsonb_build_object(
+                    'src', vd.src,
+                    'type', vt.type,
+                    'ext', vt.extension
+                )
+            ) as video_src,
+            v.jewellery_id,
+            p.name as producer
+        from
+            jw_medias.video_details as vd
+                join jw_medias.videos v on v.id = vd.video_id
+                join jw_medias.video_types vt on vd.video_type_id = vt.id
+                join jw_medias.videos on vd.video_id = videos.id
+                left join jw_medias.producers p on v.producer_id = p.id
+        group by vd.video_id, v.jewellery_id, p.name
+    )
+select
+    jsonb_agg(
+        jsonb_build_object(
+            ctv.producer, ctv.video_src
+        )
+    )
+from
+    cte_videos ctv
+        join jewelleries.jewelleries as jj on ctv.jewellery_id = jj.id
+group by jj.id
