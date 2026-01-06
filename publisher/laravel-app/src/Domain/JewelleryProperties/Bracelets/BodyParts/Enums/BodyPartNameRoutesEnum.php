8ca0d87c256d09d74d67107132a926fb4ccfa4f9
acwstudio <acwstudio2018@gmail.com>
2026-01-06 04:06:20 +0300

    a small issue was resolved

--- publisher/laravel-app/src/Domain/JewelleryProperties/Bracelets/BodyParts/Enums/BodyPartNameRoutesEnum.php
+++ publisher/laravel-app/src/Domain/JewelleryProperties/Bracelets/BodyParts/Enums/BodyPartNameRoutesEnum.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace Domain\JewelleryProperties\Bracelets\BodyParts\Enums;
 
-enum BodyPartNameRoutesEnum
+enum BodyPartNameRoutesEnum: string
 {
+    case CRUD_INDEX  = 'body-parts.index';
+    case CRUD_SHOW   = 'body-parts.show';
+    case CRUD_POST   = 'body-parts.post';
+    case CRUD_PATCH  = 'body-parts.patch';
+    case CRUD_DELETE = 'body-parts.delete';
 
+
+    case RELATED_TO_CLASPS              = 'body-parts.clasps';
+    case RELATIONSHIP_TO_CLASPS         = 'body-parts.relationships.clasps';
+    case RELATED_TO_BRACELET_BASES      = 'body-parts.bracelet-bases';
+    case RELATIONSHIP_TO_BRACELET_BASES = 'body-parts.relationships.bracelet-bases';
+    case RELATED_TO_BRACELETS           = 'body-part.bracelets';
+    case RELATIONSHIP_TO_BRACELETS      = 'body-part.relationships.bracelets';
 }
--- publisher/laravel-app/src/Domain/JewelleryProperties/Bracelets/BodyParts/Enums/BodyPartRelationshipsEnum.php
+++ publisher/laravel-app/src/Domain/JewelleryProperties/Bracelets/BodyParts/Enums/BodyPartRelationshipsEnum.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Domain\JewelleryProperties\Bracelets\BodyParts\Enums;
 
-enum BodyPartRelationshipsEnum
+enum BodyPartRelationshipsEnum: string
 {
-
+    case CLASPS         = 'clasps';
+    case BRACELET_BASES = 'braceletBases';
+    case BRACELETS      = 'bracelets';
 }
