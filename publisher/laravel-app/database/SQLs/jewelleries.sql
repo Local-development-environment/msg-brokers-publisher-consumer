1a781e13885b064a6270eeaf872155c51965a31e
acwstudio <acwstudio2018@gmail.com>
2026-01-22 14:20:19 +0300

    added archive about refactoring jewellery app, was fixed some v-jewellery issues

--- publisher/laravel-app/database/SQLs/jewelleries.sql
+++ publisher/laravel-app/database/SQLs/jewelleries.sql
@@ -213,7 +213,8 @@ with
                 'cuff_links_id', jwcl.id,
                 'cuff_links_clasp', jclc.name,
                 'cuff_links_form', jclf.name,
-                'cuff_links_type', jclt.name
+                'cuff_links_type', jclt.name,
+                'dimensions', jwcl.dimensions
             ) as spec_props,
             jwcl.quantity as quantity,
             cast(jwcl.price as decimal(10, 2)) as avg_price,
@@ -224,8 +225,8 @@ with
                 join jewelleries.jewelleries as jj on jwcl.id = jj.id
                 join jewelleries.jewellery_categories as jc on jj.jewellery_category_id = jc.id
                 join jw_properties.cuff_link_clasps as jclc on jwcl.cuff_link_clasp_id = jclc.id
-                join jw_properties.cuff_link_forms as jclf on jwcl.cuff_link_clasp_id = jclf.id
-                join jw_properties.cuff_link_types as jclt on jwcl.cuff_link_clasp_id = jclt.id
+                join jw_properties.cuff_link_forms as jclf on jwcl.cuff_link_form_id = jclf.id
+                join jw_properties.cuff_link_types as jclt on jwcl.cuff_link_type_id = jclt.id
 
         union all
 
